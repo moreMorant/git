ad9a4effe69f94e470357ba7e9c83e4fb1bbb67f
morant <916996159@qq.com>
2022-04-26 20:53:16 +0800

    hot-fix修改了文件

--- src/main/java/com/morant/GitTest.java
+++ src/main/java/com/morant/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("heihei");
+        System.out.println("hit-fix修改了");
     }
 }
