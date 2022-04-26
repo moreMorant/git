ac6f6cdb94a44aadadb492d9e664c91dd236e63c
morant <916996159@qq.com>
2022-04-26 20:55:27 +0800

    hot-fix二次修改

--- src/main/java/com/morant/GitTest.java
+++ src/main/java/com/morant/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("heihei");
         System.out.println("hit-fix修改了");
+        System.out.println("hot-fix二次修改");
     }
 }
